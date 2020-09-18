ca62b3c93e7dea9aa58a7c1fba4d1abba42cc7e1
evansmwenda <evansmwenda.em@gmail.com>
2020-09-18 16:04:04 +0300

    product resource collection created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Product;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -16,7 +17,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-        return Product::all();
+        return  ProductCollection::collection(Product::all());
     }
 
     /**
