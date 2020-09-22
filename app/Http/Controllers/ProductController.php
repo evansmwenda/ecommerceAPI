a6128d5697a6b64fba80ea9480a8679f16e63cfa
evansmwenda <evansmwenda.em@gmail.com>
2020-09-22 14:56:58 +0300

    created product with token requirements

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Model\Product;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -37,9 +42,20 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response([
+            "data" => new ProductResource($product)
+            ],Response::HTTP_CREATED);
     }
 
     /**
